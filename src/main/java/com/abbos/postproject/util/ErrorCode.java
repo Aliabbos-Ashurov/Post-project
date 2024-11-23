832b7cc65cf39eb40ab9c8c67a9e53dd2c24e7a3
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-11-23 20:35:45 +0500

    ...

---
+++ src/main/java/com/abbos/postproject/util/ErrorCode.java
@@ -0,0 +1,24 @@
+package com.abbos.postproject.util;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 19/November/2024  17:12
+ **/
+public class ErrorCode {
+    public static final String SUCCESS = "SUCCESS";
+    public static final String FAILURE = "FAILURE";
+    public static final String ERROR = "ERROR";
+
+    public static final String NOT_FOUND = "NOT_FOUND";
+    public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
+    public static final String USER_ALREADY_EXIST = "USER_ALREADY_EXIST";
+    public static final String USER_NOT_EXIST = "USER_NOT_EXIST";
+    public static final String RESOURCE_NOT_FOUND = "RESOURCE_NOT_FOUND";
+    public static final String INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
+    public static final String INVALID_REQUEST = "INVALID_REQUEST";
+    public static final String INVALID_TOKEN = "INVALID_TOKEN";
+    public static final String UNAUTHORIZED = "UNAUTHORIZED";
+    public static final String FORBIDDEN = "FORBIDDEN";
+    public static final String SESSION_EXPIRED = "SESSION_EXPIRED";
+    public static final String TOKEN_EXPIRED = "TOKEN_EXPIRED";
+}
