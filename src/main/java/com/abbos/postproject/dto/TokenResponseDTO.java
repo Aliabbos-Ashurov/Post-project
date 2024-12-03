2f16d688814d725cd21985cc1c0aeea82ce7d686
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-12-03 12:31:30 +0500

    ...

---
+++ src/main/java/com/abbos/postproject/dto/TokenResponseDTO.java
@@ -0,0 +1,27 @@
+package com.abbos.postproject.dto;
+
+import com.abbos.postproject.dto.response.Response;
+import com.abbos.postproject.enums.TokenType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 02/October/2024  13:21
+ **/
+@Validated
+@JsonPropertyOrder({"user_id", "token_type", "access", "refresh", "_links"})
+public record TokenResponseDTO(
+        @JsonProperty("token_type")
+        @NotNull TokenType tokenType,
+        @JsonProperty("user_id")
+        @NotNull Long userId,
+        @NotNull TokenDTO access,
+        @NotNull TokenDTO refresh
+) implements Response {
+    public static TokenResponseDTO of(Long userId, TokenDTO access, TokenDTO refresh) {
+        return new TokenResponseDTO(TokenType.BEARER, userId, access, refresh);
+    }
+}
