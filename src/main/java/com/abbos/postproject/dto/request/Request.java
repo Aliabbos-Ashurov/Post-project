64d2d2da2951b3d93db7e5d1d1429b25324381f7
Aliabbos Ashurov <aliabbosashurov@gmail.com>
2024-12-07 19:36:41 +0500

    ...

---
+++ src/main/java/com/abbos/postproject/dto/request/Request.java
@@ -0,0 +1,10 @@
+package com.abbos.postproject.dto.request;
+
+import com.abbos.postproject.dto.DTO;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 19/November/2024  16:55
+ **/
+public interface Request extends DTO {
+}
---
+++ src/main/java/com/abbos/postproject/dto/response/Response.java
@@ -0,0 +1,10 @@
+package com.abbos.postproject.dto.response;
+
+import com.abbos.postproject.dto.DTO;
+
+/**
+ * @author Aliabbos Ashurov
+ * @since 19/November/2024  16:54
+ **/
+public interface Response extends DTO {
+}
